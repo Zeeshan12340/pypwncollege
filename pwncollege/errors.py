31d4dd1d4303df3f64dc75dd1cbd15b65855bf31
Zeeshan1234 <mayub9799@gmail.com>
2024-02-03 19:18:54 +0500

    remove unused error exceptions

--- pwncollege/errors.py
+++ pwncollege/errors.py
@@ -4,59 +4,8 @@ class PwnException(Exception):
     pass
 
 
-class ApiError(PwnException):
-    """The API responded in an unexpected way"""
-
-
 class AuthenticationException(PwnException):
     """An error authenticating to the API"""
-
-    pass
-
-
-class NotFoundException(PwnException):
-    """The API returned a 404 response for this request"""
-
-    pass
-
-
-class MissingEmailException(AuthenticationException):
-    """An email was not given where it was required"""
-
-    pass
-
-
-class MissingPasswordException(AuthenticationException):
-    """A password was not given where it was required"""
-
-    pass
-
-
-class ChallengeException(PwnException):
-    """An error associated with a challenge"""
-
-    pass
-
-
-class TooManyRestartAttempts(ChallengeException):
-    """Error for too many restart attempts"""
-
-    pass
-
-
-
-class SolveError(PwnException):
-    """Exceptions for solving"""
-
-
-class IncorrectFlagException(SolveError):
-    """An incorrect flag was submitted"""
-
-    pass
-
-
-class FlagAlreadySubmitted(SolveError):
-    """Player has already completed the flag for this challenge"""
 
     pass
 
@@ -77,14 +26,6 @@ class IncorrectArgumentException(PwnException):
 
     pass
 
-
-class RateLimitException(PwnException):
-    """An internal ratelimit to prevent spam was violated"""
-
-    def __init__(self, message):
-        print(message)
-        super().__init__(message)
-
 class ServerErrorException(PwnException):
     """A server error occured and an endpoint return a >500 status code"""
 
@@ -93,7 +34,3 @@ class ServerErrorException(PwnException):
         super().__init__(message)
 
 
-class CacheException(PwnException):
-    """There was an issue with the token cache"""
-
-    pass
