ab2d847fda84d1d0a24fca766be3318a1df4a2b4
Zeeshan1234 <mayub9799@gmail.com>
2024-02-03 23:39:22 +0500

    another check

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,3 +4,4 @@ def test_login():
     """Tests the ability to login and receive a valid session token."""
     client = PWNClient(email='test1337', password='test1337')
     assert client._app_cookie is not None
+    assert client.user.name == 'test1337'
